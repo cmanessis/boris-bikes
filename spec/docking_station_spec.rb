0d49b7cdb7696f07d5b2b83d23e682e2bfed3074
cmanessis <cmpetrowii@gmail.com>
2015-05-09 02:01:26 +0100

    refactored docking_station unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,23 @@
+
 require 'docking_station'
 
 describe DockingStation do
-
-  let(:bike) {double :bike, working?: true}
-
-  it {is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
+    subject.dock Bike.new
+    bike = subject.release_bike
+    expect(bike.working?).to be true
+  end
+
+  it 'does not release broken bikes' do
+    bike = Bike.new
+    bike.report_broken
     subject.dock bike
-    released_bike = subject.release_bike
-    expect(released_bike.working?).to be true
-end
+    expect {subject.release_bike}.to raise_error 'No working bikes available'
+  end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
-
 
   describe 'release_bike' do
     it 'raises an error when there are no bikes available' do
@@ -23,14 +27,8 @@ end
 
   describe 'dock' do
     it 'raises an error when full' do
-     subject.capacity.times { subject.dock double :bike }
-      expect { subject.dock double :bike }.to raise_error 'Docking station full'
-    end
-  end
-
-  describe 'edit_capacity' do
-    it 'becomes edited' do
-      expect(subject.capacity = 50).to eq 50
+      subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 end
